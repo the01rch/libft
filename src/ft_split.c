c61166cfa9a4a9c64115505de94121984bbbd90e
reda <reda.rouich@epitech.eu>
2023-05-13 23:57:56 +0200

    [ADD] ft_split test

--- src/ft_split.c
+++ src/ft_split.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int nb_delim(char const *s, char c) {
     int nb = 0;
@@ -13,18 +14,57 @@ int nb_delim(char const *s, char c) {
 
 int *size_cols(char const *s, char c) {
     int rows = nb_delim(s, c);
-    int *size = malloc(sizeof(int) * rows);
+    int *size = NULL; 
     int x = 0;
 
+    if (rows == 0)
+        return NULL;
+    size = malloc(sizeof(int) * rows);
+    if (size == NULL)
+        return NULL;
     for (int y = 0; y < rows; y++) {
-        for ( ; s[x] != '\0'; x++) {
-            if (s[x] != c)
-                size[y] = x;
+        for (int u = 0 ; s[x] != '\0'; x++, u++) {
+            if (s[x++] == c) {
+                size[y] = u;
+                break;
+            }
         }
-    } 
+    }
     return size;
 }
 
 char **ft_split(char const *s, char c) {
-    return NULL; 
+    int rows = nb_delim(s, c);
+    int *cols = size_cols(s, c); 
+    char **arr = NULL;
+    int u = 0;
+
+    if (rows == 0)
+        return NULL;
+    arr = malloc(sizeof(char *) * rows + 1);
+    for (int y = 0; y < rows; y++) {
+        arr = malloc(sizeof(char) * cols[y] + 1);
+        for (int x = 0; x < cols[y]; x++) {
+            arr[y][x] = s[u];
+            u++;
+        } 
+        arr[y][cols[y]] = '\0';
+        u++;
+    }
+    arr[rows] = NULL;
+    return NULL;
+}
+
+int main(int ac, char **av) {
+    char const *str = "bleach/hxh/snk/vinland/kingdom/CSM";
+    char **arr = ft_split(str, '/');
+
+    if (arr == NULL)
+        printf("ERROR : NULL\n");
+    printf("%s", arr[0]);
+    /*
+    for (int i = 0; arr[i] != NULL; i++) {
+        printf("arr[%d] = %s\n", i, arr[i]);
+    }
+    */
 }
